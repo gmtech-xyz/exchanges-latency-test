77eabf0c05f7269a3e826d36382bda0f74611c96
iam4x <iam4x@me.com>
2023-04-02 16:35:51 +0200

    feat(seo): add title & description

--- src/root.tsx
+++ src/root.tsx
@@ -17,9 +17,13 @@ const Root = () => {
   return (
     <Html lang="en">
       <Head>
-        <Title>Exchanges Latency tester</Title>
+        <Title>Exchanges Latency Tester - Test Your Trading Speed Today</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta
+          name="description"
+          content="Improve your trading experience with our exchanges latency tester. Check your trading speed and optimize your trades with our powerful tool. Boost your trading profits now!"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
